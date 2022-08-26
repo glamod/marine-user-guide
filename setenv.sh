ca6b87a008b4ea4c87a62af67091dd334cc6abb7
Axel Andersson <aanderss@host596.jc.rl.ac.uk>
2022-08-26 09:49:25 +0100

    UPDATE :: set environment paths and modules.

--- setenv.sh
+++ setenv.sh
@@ -1,6 +1,10 @@
+# load python module
+#module load jaspy/3.7/r20181219
 
 # WATCH THIS HARCODING UNTIL WE DECIDE WHAT TO DO WITH REPOS AND ENVS!
-pyEnvironment_directory=$mug_code_directory/env/mug_env
+#pyEnvironment_directory=$mug_code_directory/env/mug_env
+pyEnvironment_directory=/gws/smf/j04/c3s311a_lot2/code/marine_code/marine-user-guide/env/mug_env
+
 # Activate python environment and add jaspy3.7 path to LD_LIBRARY_PATH so that cartopy and other can find the geos library
 source $pyEnvironment_directory/bin/activate
 export PYTHONPATH="$mug_code_directory:${PYTHONPATH}"
--- setpaths.sh
+++ setpaths.sh
@@ -1,9 +1,14 @@
-data_directory=/group_workspaces/jasmin2/glamod_marine/data
-code_directory=/gws/smf/j04/c3s311a_lot2/code/marine_code
+#data_directory=/gws/nopw/j04/glamod_marine/data
+data_directory=/work/scratch-nopw/aanderss/glamod_marine/data
+#code_directory=/gws/smf/j04/c3s311a_lot2/code/marine_code
+code_directory=/home/users/aanderss/mug/code/marine_code/
 
 mug_data_directory=$data_directory/marine-user-guide
 mug_code_directory=$code_directory/marine-user-guide
+mug_config_directory=$code_directory/glamod-marine-config/marine-user-guide
 
-echo 'Release data directory is '$data_directory
-echo 'Marine User guide data directory is '$mug_data_directory
-echo 'User guide code directory is '$mug_code_directory
+echo 'Release data directory is:             '$data_directory
+echo 'Marine User guide data directory is:   '$mug_data_directory
+echo 'Marine User guide code directory is:   '$mug_code_directory
+echo 'Marine User guide config directory is: '$mug_config_directory
+
