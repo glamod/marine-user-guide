620e3416a4b9fa10492e42e1ce92343050cdf2d8
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2024-11-18 11:59:57 +0000

    bastion paths

--- setpaths.sh
+++ setpaths.sh
@@ -1,11 +1,7 @@
-#data_directory=/gws/nopw/j04/glamod_marine/data
-#data_directory=/work/scratch-nopw/aanderss/glamod_marine/data
-export data_directory=/ichec/work/glamod/data/marine
-#code_directory=/gws/smf/j04/c3s311a_lot2/code/marine_code
-#code_directory=/home/users/aanderss/mug/code/marine_code/
-export code_directory=/ichec/work/glamod/marine-user-guide.2022
+export data_directory=/ichec/work/glamod/data/marine/marine
+export code_directory=/ichec/work/glamod/glamod_marine/marine-user-guide
 
-export mug_data_directory=$data_directory/marine-user-guide.2022
+export mug_data_directory=$data_directory/marine-user-guide
 export mug_code_directory=$code_directory
 export mug_config_directory=$mug_code_directory/config
 
