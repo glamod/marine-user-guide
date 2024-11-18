fab32e34345d3e5be3811320f32eee94262be301
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2024-11-18 15:00:06 +0000

    python3 -Y python

--- figures_sd/plot_all_sd.sh
+++ figures_sd/plot_all_sd.sh
@@ -23,13 +23,13 @@ while read sid_dck; do
   log_diri=${mug_data_directory}/${version}/level2/log/${sid_dck}
 
   echo '${sid_dck}: start plotting'
-  python3 ecv_reports_ts_plot_grid_sd.py ${sid_dck} $mug_conf_dir_fig/ecv_reports_ts_plot_grid_sd-all.json > ${log_diri}/ecv_reports_ts_plot_grid_sd-all.log 2>&1 &
-  python3 ecv_reports_ts_plot_grid_sd.py ${sid_dck} $mug_conf_dir_fig/ecv_reports_ts_plot_grid_sd-optimal.json > ${log_diri}/ecv_reports_ts_plot_grid_sd-optimal.log 2>&1 &
-  python3 nreports_dup_ts_sd.py ${sid_dck} $mug_conf_dir_fig/nreports_dup_ts_sd.json > ${log_diri}/nreports_dup_ts_sd.log 2>&1 &
-  python3 nreports_qc_ts_sd.py ${sid_dck} $mug_conf_dir_fig/nreports_qc_ts_sd.json > ${log_diri}/nreports_qc_ts_sd.log 2>&1 & 
-  python3 param_lat_bands_ts.py ${sid_dck} all $mug_conf_dir_fig/param_lat_bands_ts.json > ${log_diri}/param_lat_bands_all_ts.log 2>&1 &
-  python3 param_lat_bands_ts.py ${sid_dck} optimal $mug_conf_dir_fig/param_lat_bands_ts.json > ${log_diri}/param_lat_bands_optimal_ts.log 2>&1 &
-  python3 report_io_plot_sd.py ${sid_dck} $mug_conf_dir_fig/report_io_plot_sd.json > ${log_diri}/report_io_plot_sd.log 2>&1 & 
+  python ecv_reports_ts_plot_grid_sd.py ${sid_dck} $mug_conf_dir_fig/ecv_reports_ts_plot_grid_sd-all.json > ${log_diri}/ecv_reports_ts_plot_grid_sd-all.log 2>&1 &
+  python ecv_reports_ts_plot_grid_sd.py ${sid_dck} $mug_conf_dir_fig/ecv_reports_ts_plot_grid_sd-optimal.json > ${log_diri}/ecv_reports_ts_plot_grid_sd-optimal.log 2>&1 &
+  python nreports_dup_ts_sd.py ${sid_dck} $mug_conf_dir_fig/nreports_dup_ts_sd.json > ${log_diri}/nreports_dup_ts_sd.log 2>&1 &
+  python nreports_qc_ts_sd.py ${sid_dck} $mug_conf_dir_fig/nreports_qc_ts_sd.json > ${log_diri}/nreports_qc_ts_sd.log 2>&1 & 
+  python param_lat_bands_ts.py ${sid_dck} all $mug_conf_dir_fig/param_lat_bands_ts.json > ${log_diri}/param_lat_bands_all_ts.log 2>&1 &
+  python param_lat_bands_ts.py ${sid_dck} optimal $mug_conf_dir_fig/param_lat_bands_ts.json > ${log_diri}/param_lat_bands_optimal_ts.log 2>&1 &
+  python report_io_plot_sd.py ${sid_dck} $mug_conf_dir_fig/report_io_plot_sd.json > ${log_diri}/report_io_plot_sd.log 2>&1 & 
   
   sleep 1
   #make this more gentle for login node
